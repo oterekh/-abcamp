30ed77d77aa1950ddcee2f74fc5b6918ba2e7e74
Oleh <oleh.tereh@gmail.com>
2016-11-06 20:18:48 +0000

    Fixed carousel

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 	// create the module and name it tolApp
-	var tolApp = angular.module('scotchApp', ['ngRoute']);
+	var tolApp = angular.module('tolApp', ['ngRoute']);
 
 	// configure our routes
 	tolApp.config(function($routeProvider) {
@@ -31,7 +31,7 @@
 	});
 
 	// create the controller and inject Angular's $scope
-	tolApp.controller('mainController', function($scope) {
+	tolApp.controller('MainController', function($scope) {
 		// create a message to display in our view
 		$scope.message = 'Everyone come and see how good I look!';
 	});
@@ -46,4 +46,63 @@
 	
 		tolApp.controller('helpController', function($scope) {
 		$scope.message = 'Help will be there.';
+	});
+	
+	angular.module('ui.bootstrap.demo', ['ngAnimate', 'ngSanitize', 'ui.bootstrap']);
+	angular.module('ui.bootstrap.demo').controller('CarouselDemoController', function ($scope) {
+	 $scope.myInterval = 5000;
+	  $scope.noWrapSlides = false;
+	  $scope.active = 0;
+	  var slides = $scope.slides = [];
+	  var currIndex = 0;
+	
+	  $scope.addSlide = function() {
+	    var newWidth = 600 + slides.length + 1;
+	    slides.push({
+	      image: '//unsplash.it/' + newWidth + '/300',
+	      text: ['Nice image','Awesome photograph','That is so cool','I love that'][slides.length % 4],
+	      id: currIndex++
+	    });
+	  };
+	
+	  $scope.randomize = function() {
+	    var indexes = generateIndexesArray();
+	    assignNewIndexesToSlides(indexes);
+	  };
+	
+	  for (var i = 0; i < 4; i++) {
+	    $scope.addSlide();
+	  }
+	
+	  // Randomize logic below
+	
+	  function assignNewIndexesToSlides(indexes) {
+	    for (var i = 0, l = slides.length; i < l; i++) {
+	      slides[i].id = indexes.pop();
+	    }
+	  }
+	
+	  function generateIndexesArray() {
+	    var indexes = [];
+	    for (var i = 0; i < currIndex; ++i) {
+	      indexes[i] = i;
+	    }
+	    return shuffle(indexes);
+	  }
+	
+	  // http://stackoverflow.com/questions/962802#962890
+	  function shuffle(array) {
+	    var tmp, current, top = array.length;
+	
+	    if (top) {
+	      while (--top) {
+	        current = Math.floor(Math.random() * (top + 1));
+	        tmp = array[current];
+	        array[current] = array[top];
+	        array[top] = tmp;
+	      }
+	    }
+	
+	    return array;
+	  }
 	});
