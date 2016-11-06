9c9b4527f4359dab455d9c3de6fb51b3ad82939c
Oleh <oleh.tereh@gmail.com>
2016-11-06 19:06:20 +0000

    Changed controller name and fixed routr issue

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-// create the module and name it scotchApp
-	var scotchApp = angular.module('scotchApp', ['ngRoute']);
+// create the module and name it tolApp
+	var tolApp = angular.module('scotchApp', ['ngRoute']);
 
 	// configure our routes
-	scotchApp.config(function($routeProvider) {
+	tolApp.config(function($routeProvider) {
 		$routeProvider
 
 			// route for the home page
@@ -31,19 +31,19 @@
 	});
 
 	// create the controller and inject Angular's $scope
-	scotchApp.controller('mainController', function($scope) {
+	tolApp.controller('mainController', function($scope) {
 		// create a message to display in our view
 		$scope.message = 'Everyone come and see how good I look!';
 	});
 
-	scotchApp.controller('aboutController', function($scope) {
+	tolApp.controller('aboutController', function($scope) {
 		$scope.message = 'Look! I am an about page.';
 	});
 
-	scotchApp.controller('contactController', function($scope) {
+	tolApp.controller('contactController', function($scope) {
 		$scope.message = 'Contact us! JK. This is just a demo.';
 	});
 	
-		scotchApp.controller('helpController', function($scope) {
+		tolApp.controller('helpController', function($scope) {
 		$scope.message = 'Help will be there.';
 	});
